df423a1ecbf66aca95476a937da7691b10c43808
Mr.Q <thanhquangdz2020@gmail.com>
2026-04-29 23:15:47 +0700

    Update language.py

--- language.py
+++ language.py
@@ -2,33 +2,33 @@
 
 LANGUAGES = {
     "Tiếng Việt": {
-        "title": "🧠 Máy Ép Kiến Thức V6.0",
-        "header_config": "⚙️ Cấu hình máy ép",
+        "title": "🧠 Máy Ép Kiến Thức V1.36",
+        "header_config": "⚙️ Cấu hình ",
         "api_label": "Nhập DeepSeek API Key",
-        "subject_label": "🎯 Chọn chuyên ngành",
+        "subject_label": "🎯 Menu",
         "detail_label": "Độ chi tiết",
         "creative_label": "Độ sáng tạo",
         "upload_label": "Kéo thả file hoặc ảnh (png, jpg, jpeg,pptx,pdf)",
         "btn_start": "🚀 BẮT ĐẦU ÉP KIẾN THỨC",
-        "btn_download": "📥 Tải bộ phao (.md)",
+        "btn_download": "📥 Tải tài liệu (.md)",
         "success_msg": "🎉 Đã ép xong toàn bộ tài liệu!",
         "error_api": "Vui lòng cung cấp API Key ở Sidebar!",
         "status_processing": "⚡ Đang ép Slide",
         "subject_list": {
-            "Vạn năng (Bản PRO)": "Vạn năng (Bản PRO)",
+            "Vạn năng (Bản PRO)": "Vạn Năng (PRO VER.)",
             "AI & Data (HOT)": "AI & Data (HOT)",
             "Cybersecurity": "Cybersecurity",
-            "Marketing & Growth": "Marketing & Growth",
+            "Marketing & Growth": "Marketing",
             "Tâm lý học": "Tâm lý học",
             "Product & Startup": "Product & Startup",
             "DevOps & System Design": "DevOps & System Design"
         }
     },
     "English": {
-        "title": "🧠 Knowledge Crusher V6.0",
+        "title": "🧠 Knowledge Crusher V1.36",
         "header_config": "⚙️ Configuration",
         "api_label": "Enter DeepSeek API Key",
-        "subject_label": "🎯 Select Subject",
+        "subject_label": "🎯 Menu",
         "detail_label": "Detail Level",
         "creative_label": "Creativity",
         "upload_label": "Drag adn drop file, image here (png, jpg, jpeg,pptx,pdf)",
@@ -41,7 +41,7 @@ LANGUAGES = {
             "Vạn năng (Bản PRO)": "General (PRO Version)",
             "AI & Data (HOT)": "AI & Data (Trending)",
             "Cybersecurity": "Cybersecurity Expert",
-            "Marketing & Growth": "Marketing & Growth",
+            "Marketing & Growth": "Marketing ",
             "Tâm lý học": "Applied Psychology",
             "Product & Startup": "Product & Startup",
             "DevOps & System Design": "DevOps & Systems"
